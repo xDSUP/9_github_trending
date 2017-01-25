4c6f2eafad4535ab3bbf2726c2ab723096fcecc8
xDSUP <ilya.elfimow@yandex.ru>
2017-01-25 10:30:12 +0300

    ver 1.2 fix

--- github_trending.py
+++ github_trending.py
@@ -2,20 +2,19 @@ import requests
 from datetime import datetime, timedelta
 
 
-def get_trending_repositories():
+def get_trending_repositories(days_amount, page_count):
     user_datetime = datetime.now()
-    days_in_week = 7
-    github_datetime = user_datetime - timedelta(days=days_in_week)
+    github_datetime = user_datetime - timedelta(days=days_amount)
     github_datetime_string = github_datetime.strftime("%Y-%m-%dT%H:%M:%S")
     github_url = "https://api.github.com/search/repositories"
     payload = {'q': '{}{}'.format('created:>=', github_datetime_string),
-               'sort': 'stars', 'order': 'desc', 'per_page': 20}
+               'sort': 'stars', 'order': 'desc', 'per_page': page_count}
     trending_repositories_data = requests.get(github_url, params=payload)
     return trending_repositories_data.json()['items']
 
 
-def print_trending_repositories(trending_repositories):
-    print("Самые популярные репозитории за неделю :")
+def print_trending_repositories(trending_repositories, days_amount):
+    print("Самые популярные репозитории за %s дней:" % days_amount)
     for repo in trending_repositories:
         print(u'\nРепозиторий {}:\nЗвезд:{}; Открытых задач:{}'
               '\nСсылка :{}'.format(repo['name'],
@@ -24,5 +23,7 @@ def print_trending_repositories(trending_repositories):
                                     repo['html_url']))
 
 if __name__ == '__main__':
-    trending_repositories = get_trending_repositories()
-    print_trending_repositories(trending_repositories)
+    days_amount = 7
+    page_count = 20
+    trending_repositories = get_trending_repositories(days_amount, page_count)
+    print_trending_repositories(trending_repositories, days_amount)
